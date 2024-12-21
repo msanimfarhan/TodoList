1dd4b813a553bfcd71b80ae9a708c07f0d741812
mfarhan <md254274@dal.ca>
2024-12-20 20:26:38 -0400

    Project completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Todo } from './Components/Todo';
+
+
+
+export const App = () => {
+ 
+  return (
+    <div>
+      
+      <Todo/>
+      
+    </div>
+  )
+}
+export default App;
